4d250a89e86c58424cca1f71c9745d1a40a12547
Carsten Walther <carsten.walther@student.hpi.de>
2018-02-19 16:23:37 +0100

    refactoring

--- LoggerApp.py
+++ LoggerApp.py
@@ -1,3 +1,4 @@
+import argparse
 import collections
 import csv
 import json
@@ -8,7 +9,7 @@ import hashlib
 import base64
 
 import pandas as pd
-from flask import Flask, send_from_directory, Response, request
+from flask import Flask, send_from_directory, request
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 from kafka import KafkaConsumer
@@ -16,7 +17,6 @@ from kafka import TopicPartition
 
 app = Flask(__name__, static_url_path='')
 CORS(app)
-# socketio = SocketIO(app, logger=True, engineio_logger=True)
 socketio = SocketIO(app)
 
 kafka_endpoint = os.getenv('KAFKA_URL', 'vm-mpws2016hp1-05.eaalab.hpi.uni-potsdam.de:9092')
@@ -28,7 +28,7 @@ topics = ['addOffer', 'buyOffer', 'profit', 'updateOffer', 'updates', 'salesPerM
 
 
 class KafkaHandler(object):
-    def __init__(self):
+    def __init__(self, kafka_endpoint: str):
         self.consumer = KafkaConsumer(bootstrap_servers=kafka_endpoint)
         self.dumps = {}
         end_offset = {}
@@ -72,22 +72,15 @@ class KafkaHandler(object):
         self.consumer.close()
 
 
-kafka = KafkaHandler()
+kafka_handler = KafkaHandler(kafka_endpoint)
 
 
 @socketio.on('connect', namespace='/')
 def on_connect():
-    global kafka
-    if kafka.dumps:
-        for msg_topic in kafka.dumps:
-            messages = list(kafka.dumps[msg_topic])
+    if kafka_handler.dumps:
+        for msg_topic in kafka_handler.dumps:
+            messages = list(kafka_handler.dumps[msg_topic])
             emit(msg_topic, messages, namespace='/')
-
-
-def json_response(obj):
-    js = json.dumps(obj)
-    resp = Response(js, status=200, mimetype='application/json')
-    return resp
 
 
 @app.route("/topics")
@@ -98,31 +91,31 @@ def get_topics():
 @app.route("/status")
 def status():
     status_dict = {}
-    for topic in kafka.dumps:
+    for topic in kafka_handler.dumps:
         status_dict[topic] = {
-            'messages': len(kafka.dumps[topic]),
-            'last_message': kafka.dumps[topic][-1] if kafka.dumps[topic] else ''
+            'messages': len(kafka_handler.dumps[topic]),
+            'last_message': kafka_handler.dumps[topic][-1] if kafka_handler.dumps[topic] else ''
         }
     return json.dumps(status_dict)
 
 
 @app.route("/export/data")
 def export_csv():
-    consumer2 = KafkaConsumer(consumer_timeout_ms=3000, bootstrap_servers=kafka_endpoint)
+    consumer = KafkaConsumer(consumer_timeout_ms=3000, bootstrap_servers=kafka_endpoint)
 
     topic_partitions = [TopicPartition(topic, 0) for topic in topics]
-    consumer2.assign(topic_partitions)
-    consumer2.seek_to_beginning()
+    consumer.assign(topic_partitions)
+    consumer.seek_to_beginning()
 
     filename = int(time.time())
     filepath = 'data/' + str(filename) + '.csv'
     with open(filepath, 'wt', newline='') as csv_file:
         writer = csv.writer(csv_file)
-        for msg in consumer2:
+        for msg in consumer:
             msg2 = json.loads(msg.value.decode('utf-8'))
             writer.writerow([msg.topic, msg.timestamp, msg2])
 
-    consumer2.close()
+    consumer.close()
     return json.dumps({"url": filepath})
 
 
@@ -154,8 +147,9 @@ def market_situation_shaper(list_of_msgs):
     return pd.DataFrame(expanded_offers)
 
 
-def calculate_id(token):
+def calculate_id(token: str) -> str:
     return base64.b64encode(hashlib.sha256(token.encode('utf-8')).digest()).decode('utf-8')
+
 
 @app.route("/export/data/<path:topic>")
 def export_csv_for_topic(topic):
@@ -163,57 +157,57 @@ def export_csv_for_topic(topic):
         'marketSituation': market_situation_shaper
     }
 
-    auth_header = request.headers['Authorization'] if 'Authorization' in request.headers else None
-    _auth_type, merchant_token = auth_header.split(' ') if auth_header else (None, None)
+    auth_header = request.headers.get('Authorization')
+    merchant_token = auth_header.split(' ')[-1] if auth_header else None
     merchant_id = calculate_id(merchant_token) if merchant_token else None
 
     max_messages = 10**5
 
+    if topic not in topics:
+        return json.dumps({'error': 'unknown topic'})
+
     try:
-        if topic in topics:
-            consumer = KafkaConsumer(consumer_timeout_ms=1000, bootstrap_servers=kafka_endpoint)
-            topic_partition = TopicPartition(topic, 0)
-            consumer.assign([topic_partition])
+        consumer = KafkaConsumer(consumer_timeout_ms=1000, bootstrap_servers=kafka_endpoint)
+        topic_partition = TopicPartition(topic, 0)
+        consumer.assign([topic_partition])
 
-            consumer.seek_to_beginning()
-            start_offset = consumer.position(topic_partition)
+        consumer.seek_to_beginning()
+        start_offset = consumer.position(topic_partition)
 
-            consumer.seek_to_end()
-            end_offset = consumer.position(topic_partition)
+        consumer.seek_to_end()
+        end_offset = consumer.position(topic_partition)
 
-            filename = topic + '_' + str(int(time.time()))
-            filepath = 'data/' + filename + '.csv'
+        filename = topic + '_' + str(int(time.time()))
+        filepath = 'data/' + filename + '.csv'
 
-            msgs = []
+        msgs = []
+        '''
+        Assumption: message offsets are continuous.
+        Start and end can be anywhere, end - start needs to match the amount of messages.
+        TODO: when deletion of some individual messages is possible and used, refactor!
+        '''
+        offset = max(start_offset, end_offset - max_messages)
+        consumer.seek(topic_partition, offset)
+        for msg in consumer:
             '''
-            Assumption: message offsets are continuous.
-            Start and end can be anywhere, end - start needs to match the amount of messages.
-            TODO: when deletion of some individual messages is possible and used, refactor!
+            Don't handle steadily incoming new messages
+            only iterate to last messages when requested
             '''
-            offset = max(start_offset, end_offset - max_messages)
-            consumer.seek(topic_partition, offset)
-            for msg in consumer:
-                '''
-                Don't handle steadily incoming new messages
-                only iterate to last messages when requested
-                '''
-                if offset >= end_offset:
-                    break
-                offset += 1
-                try:
-                    msg_json = json.loads(msg.value.decode('utf-8'))
-                    # filtering on messages that can be filtered on merchant_id
-                    if 'merchant_id' not in msg_json or msg_json['merchant_id'] == merchant_id:
-                        msgs.append(msg_json)
-                except ValueError as e:
-                    print('ValueError', e, 'in message:\n', msg.value)
-            consumer.close()
+            if offset >= end_offset:
+                break
+            offset += 1
+            try:
+                msg_json = json.loads(msg.value.decode('utf-8'))
+                # filtering on messages that can be filtered on merchant_id
+                if 'merchant_id' not in msg_json or msg_json['merchant_id'] == merchant_id:
+                    msgs.append(msg_json)
+            except ValueError as e:
+                print('ValueError', e, 'in message:\n', msg.value)
+        consumer.close()
 
-            df = shaper[topic](msgs) if topic in shaper else pd.DataFrame(msgs)
-            df.to_csv(filepath, index=False)
-            response = {'url': filepath}
-        else:
-            response = {'error': 'unknown topic'}
+        df = shaper[topic](msgs) if topic in shaper else pd.DataFrame(msgs)
+        df.to_csv(filepath, index=False)
+        response = {'url': filepath}
     except Exception as e:
         response = {'error': 'failed with: ' + str(e)}
 
@@ -225,5 +219,12 @@ def static_proxy(path):
     return send_from_directory('data', path, as_attachment=True)
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Kafka Reverse Proxy')
+    parser.add_argument('--port', type=int, default=8001, help='port to bind socketIO App to')
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=8001)
+    args = parse_arguments()
+    socketio.run(app, host='0.0.0.0', port=args.port)
